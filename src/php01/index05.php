25cd58f7a89bd54e7772fc8d26947179eff1e709
yayo-sakura <leon.t18041y@icloud.com>
2025-01-26 11:16:18 +0900

    リモートリポジトリの変更

---
+++ src/php01/index05.php
@@ -0,0 +1,10 @@
+<?php
+$count =0 ;
+
+while ($i <= 100){
+    if ($i === 20) {
+        break;
+    }
+    echo $i . "<br />";
+    $i++;
+}
---
+++ src/php01/index08.php
@@ -0,0 +1,23 @@
+<?php
+$people = [
+  [
+    "last_name" => "山田",
+    "first_name" => "太郎",
+    "age" => 29,
+    "gender" => "男性"
+  ],
+  [
+    "last_name" => "鈴木",
+    "first_name" => "次郎",
+    "age" => 25,
+    "gender" => "男性"
+  ],
+  [
+    "last_name" => "佐藤",
+    "first_name" => "花子",
+    "age" => 20,
+    "gender" => "女性"
+  ]
+];
+
+echo $people[0]["last_name"];
